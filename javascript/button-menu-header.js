7497c6f9c724f179aacc5af4fb22d6eb8aa4bb58
GabrielM100 <123251177+GabrielM100@users.noreply.github.com>
2024-06-07 20:25:08 -0300

    criando menu hamburguer em javascript

---
+++ javascript/button-menu-header.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button-menu-header');
+const menu = document.getElementById('menu');
+
+function clickAnimation(){
+    button.classList.toggle('active');
+    menu.classList.toggle('active');
+}
