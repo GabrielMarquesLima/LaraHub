5bab277978bb261dce153f092a6b324478be52cb
GabrielM100 <123251177+GabrielM100@users.noreply.github.com>
2024-06-07 21:31:44 -0300

    adicionando watson

--- javascript/watson.js
+++ javascript/watson.js
@@ -1,4 +1,5 @@
-window.watsonAssistantChatOptions = {
+
+  window.watsonAssistantChatOptions = {
     integrationID: "dbfc0eec-373e-4db3-97ad-28cbf25abe1e", // The ID of this integration.
     region: "us-east", // The region your integration is hosted in.
     serviceInstanceID: "5c3877d5-96ec-4a11-8330-58c3fcbd18c3", // The ID of your service instance.
